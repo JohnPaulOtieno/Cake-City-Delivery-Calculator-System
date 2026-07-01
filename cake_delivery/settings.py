547cb3b42386e66c22550fb50628cfe64344dfd9
JohnPaulOtieno <johnpaul9667@gmail.com>
2026-07-01 12:44:15 +0300

    Security Feature Update

--- cake_delivery/settings.py
+++ cake_delivery/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-8tf-m(pbejku$f&q9%q$i60f(+@8-4q-%+!&#9#lwkp*=6rg_s')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 
